41391db9e38ab68cda66e27ae38fb4249d9ebbe2
505 <505@academicos.cesde.edu.co>
2021-09-03 19:27:00 -0500

    se crea archivo

---
+++ src/arreglos/Arreglos.java
@@ -0,0 +1,37 @@
+
+package arreglos;
+import java.util.Scanner;
+
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        Scanner entrada = new Scanner(System.in);
+        
+        String prod1 = "cocacola";
+        String prod2 = "papa";
+        String prod3 = "arroz";
+        String prod4 = "cañon de cerdo";
+        String prod5 = "pansitos";
+        String prod6 = "piña";
+        
+        int [] numeros= new int[4];
+    
+        
+            for(int i = 0; i<numeros.length; i++ ){
+
+                System.out.println("digita el dato:" +i);
+                numeros[i] = entrada.nextInt();
+                
+                System.out.println("todos los datos son" + numeros[i]);
+
+            }
+
+        }
+        
+    }
+    
+
